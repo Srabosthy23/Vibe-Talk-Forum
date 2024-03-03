b81fe0d4bcf2784887217b6cc1fd4bea4c4ea8b5
Srabosthy23e <srabosthycse@gmail.com>
2024-03-03 12:04:28 +0600

    lets discuss section dynamically post info fetched

--- retro.js
+++ retro.js
@@ -6,56 +6,52 @@ const loadAllPost = async () => {
     console.log(posts)
 
     const postContainer = document.getElementById('post-container')
-    const readPost = document.getElementById('read-post')
+    
 
     posts.forEach((post) => {
             // console.log(post)
             const postInfo = document.createElement("div");
             postInfo.classList.add('flex')
             postInfo.innerHTML = `
-            <div  class="flex">
-                <div class="bg-slate-100 shadow-xl rounded-2xl flex gap-5 border p-5">
+            <div  class="flex ">
+                <div class="bg-slate-100 shadow-xl rounded-2xl flex gap-5 border p-5 lg:w-[550px]">
                     <div class="relative">
                         <img class=" h-10 lg:h-16 w-28 lg:w-16 rounded-2xl"
-                        src="https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg" alt="">
-                        <div class="absolute top-0 right-0">
-                            <input type="radio" name="radio-2" class="radio radio-success" checked />
-                        </div>
+                        src="${post.image}" alt="">
+                        ${post.isActive? '<div id="active" class="absolute top-0 right-0 w-4 h-4 bg-green-400 rounded-full border"></div>' : '<div id="active" class="absolute top-0 right-0 w-4 h-4 bg-red-600 rounded-full border"></div>'}
                     </div>
                     <div>
                         <div class="flex gap-5 font-semibold">
                             <div class="flex gap-1">
                                 <p>#</p>
-                                <p>music</p>
+                                <p>${post.category}</p>
                             </div>
                         <div>
-                        <p><span>Author:  </span>Alwad hossain</p>
+                        <p><span>Author: </span>${post.author.name}</p>
                     </div>
                 </div>
-                        <h2 class="card-title">10 kids unaware of their hallowen kdfkdf </h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eveniet magnam fuga nulla
-                            eligendi minus.</p>
+                        <h2 class="card-title">${post.title}</h2>
+                        <p>${post.description}</p>
                         <hr class="mt-5 mb-5 border-dashed border-2">
                         <div class="flex justify-between">
                         <!-- comment, view, type -->
                             <div class="flex gap-7">
                                 <div class="flex items-center gap-3">
                                     <i class="fa-regular fa-comment"></i>
-                                    <p>560</p>
+                                    <p>${post.comment_count}</p>
                                 </div>
                                 <div class="flex items-center gap-3">
                                     <i class="fa-solid fa-eye"></i>
-                                    <p>1566</p>
+                                    <p>${post.view_count}</p>
                                 </div>
                                 <div class="flex items-center gap-3">
                                     <i class="fa-regular fa-clock"></i>
-                                    <p>1566</p>
+                                    <p>${post.posted_time}</p>
                                 </div>
-                        </div>
+                            </div>
                         <!-- read post -->
                         <div>
-                            <i class="fa-regular fa-envelope text-2xl"></i>
+                            <button onclick="check(${post.id})" class ="btn"><i class="fa-regular fa-envelope text-2xl"></i></button>    
                         </div>
                     </div>
                 </div>
@@ -63,18 +59,30 @@ const loadAllPost = async () => {
         </div>            
         `
         postContainer.appendChild(postInfo);
-        const readInfo = document.createElement("div");
-        readInfo.className = "flex justify-between p-5 rounded-2xl shadow-xl bg-white m-2"
-        readInfo.innerHTML = `
-            <h2>10 kids unware of their fate</h2>
-            <div class="flex gap-2 items-center">
-                <i class="fa-solid fa-eye"></i>
-                <p>1564</p>
-            </div>
-        `
-        readPost.appendChild(readInfo)
-        })
+          // active status
+          const active = document.getElementById('active')
+          const status = post.isActive
+          console.log(status)
+          if(status === true){
+            active.style.backgroundColor = "green";
+          }
+          else{
+            active.style.backgroundColor = "red";
+          }
+        //   if(status === false){
+        //       active.style.backgroundColor = "red";
+        //   }
+        
+        
+        // const readPost = document.getElementById('read-post')
+              
+    })
 }
 
 // call function
 loadAllPost()
+
+// read info function check call
+const check = (id) =>{
+            console.log(id)
+         } 
